554cd1ac97c5dc7aa019362b33f22d5749b8ad38
Guilherme <guigododoti@gmail.com>
2025-08-11 11:51:51 -0300

    .
    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import CadastroProduto from "./pages/CadastroProduto";
-import ListaEstoque from "./pages/ListaEstoque";
-import RegistroVenda from "./pages/RegistroVenda";
-import Relatorios from "./pages/Relatorios";
-import Navbar from "./components/Navbar";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// Importa suas páginas
+import CadastroProduto from './pages/CadastroProduto';
+import ListaEstoque from './pages/ListaEstoque';
+import RegistroVenda from './pages/RegistroVenda';
+// import Relatorios from './pages/Relatorios';
+// import Login from './pages/Login';
+
+// Importa sua barra de navegação
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/cadastro" element={<CadastroProduto />} />
-        <Route path="/estoque" element={<ListaEstoque />} />
-        <Route path="/venda" element={<RegistroVenda />} />
-        <Route path="/relatorios" element={<Relatorios />} />
-      </Routes>
+      {/* NavBar aparecerá em todas as páginas */}
+      <NavBar /> 
+
+      <div className="container">
+        <Routes>
+          {/* Defina a rota padrão para a lista de estoque */}
+          <Route path="/" element={<ListaEstoque />} />
+          <Route path="/estoque" element={<ListaEstoque />} />
+          <Route path="/cadastrar-produto" element={<CadastroProduto />} />
+          <Route path="/registrar-venda" element={<RegistroVenda />} />
+
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: "http://localhost:5000",
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001', 
 });
+
+
+api.interceptors.request.use(async (config) => {
+  const token = localStorage.getItem('token'); // Pega o token salvo no navegador
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
